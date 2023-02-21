70587e94b504643c5a25c53f8540884cd0df9bd5
Adam Ra <adamra@3c-06-30-16-93-bf.wireless.temple.edu>
2023-02-21 12:39:04 -0500

    Fixed up Switch Cases and testing out reading into proc filesystem

--- myps.c
+++ myps.c
@@ -1,16 +1,36 @@
+// Adam Ra
+// Purpose : A parser to read into the /proc filesystem
+// Lab 01
+
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int main(int argc, char *argv[])
 {
-	int opt;
-	
-	while((opt = getopt(argc, argv, "psUSvc")) != -1)
+	int opt, unused, ppid;
+    char filename[1000];
+    char comm[1000];
+    char state;
+    
+    if (argc == 1)
+        printf("No parameters\n");
+
+	while((opt = getopt(argc, argv, "p:sUSvc")) != -1)
 	{
 		switch(opt)
 		{
 			case 'p':
-                printf("p\n");
+                printf("pid = %s\n",optarg);
+                sprintf(filename,"/proc/%s/stat",optarg);
+                FILE *f = fopen(filename, "r");
+                fscanf(f, "%d %s %c %d", &unused, comm, &state, &ppid);
+                printf("comm = %s\n", comm);
+                printf("state = %c\n", state);
+                printf("parent pid = %d\n", ppid);
+                fclose(f);
                 break;
             case 's':
                 printf("s\n");
