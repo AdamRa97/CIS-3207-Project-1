3b447981874cdb6bfdc0d3125bd42d71d3744457
AdamRa97 <adamra97@gmail.com>
2023-03-01 09:49:33 -0500

    No flags given; prints out all the PID's in /proc that matches with current UID

--- myps.c
+++ myps.c
@@ -7,49 +7,85 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <dirent.h>
+#include <string.h>
 
-// void parseFile(char *optarg);
-
-int opt, unused, ppid, pgrp, session, tty_nr;
+int opt, unused, pid, ppid, pgrp, session, tty_nr, uid;
 unsigned long long utime;
 char filename[1000];
 char comm[1000];
+char status_path[1000];
+char line[1000];
 char state;
 
-int main(int argc, char *argv[])
-{
-    
+int main(int argc, char *argv[]){
+
+    // If there are no flags then just print out everything normally
     if (argc == 1){
-        // printf("No parameters\n");
-        // To get current PID for program
-        pid_t curr_pid = getpid();
-        sprintf(filename,"/proc/%d/stat",curr_pid);
-        FILE *f = fopen(filename, "r");
-        fscanf(f, "%d %s %c %d %d %d %d", &unused, comm, &state, &ppid, &pgrp, &session, &tty_nr);
-        fclose(f);
+        // To enter the '/proc' file
+        DIR* dir = opendir("/proc");
+        if (dir == NULL){
+            perror("opendir");
+            return 1;
+        }
 
-        FILE *self = fopen("/proc/%d/stat", "r");
-        if (fscanf(self, "%*d %*s %*c %*d %*d %*d %*d %*d %*u %*u %*u %*u %llu", &utime) == 1)
-            printf("My utime is: %llu\n", utime);
-        fclose(self);
+        struct dirent* entry;
+        while ((entry = readdir(dir)) != NULL){
+            char* name = entry->d_name;
+            // If it's not a PID directory then skip it
+            if (name[0] < '0' || name[0] > '9')
+                continue;
 
-        printf("PID:    %d | ", curr_pid);
-        printf("utime:  %llu |\n", utime);
+            // Read contents of /proc/<PID>/status file
+            snprintf(status_path, sizeof(status_path), "/proc/%s/status", name);
+            FILE* status_file = fopen(status_path, "r");
+            if (status_file == NULL)
+                continue;
 
+            // Extract information from the status file
+            while (fgets(line, sizeof(line), status_file) != NULL){
+                if (strncmp(line, "Uid:", 4) == 0){
+                    sscanf(line + 4, "%*s %d", &uid);
+                    break;
+                }
+            }
+            fclose(status_file);
+
+            // Read contents of /proc/<PID>/stat file
+            char stat_path[128];
+            snprintf(stat_path, sizeof(stat_path), "/proc/%s/stat", name);
+            FILE* stat_file = fopen(stat_path, "r");
+            if (stat_file == NULL) {
+                // Couldn't open file, skip
+                continue;
+            }
+
+            // Parse the first field (the PID) from the stat file
+            fscanf(stat_file, "%d %s %c", &pid, comm, &state);
+            fclose(stat_file);
+
+            // Print out information about the process
+            if (uid == getuid())
+                printf("PID: %d, Command: %s, State: %c, UID: %d\n", pid, comm, state, uid);
+        }
+
+        closedir(dir);
     }
 
+
+    // Going through the flags given via case switches
 	while((opt = getopt(argc, argv, "p:sUSvc")) != -1)
 	{
 		switch(opt)
 		{
 			case 'p':
-                // parseFile(optarg)
                 // Accessing the file location and reading through it
+
                 sprintf(filename,"/proc/%s/stat",optarg);
                 FILE *f = fopen(filename, "r");
-                fscanf(f, "%d %s %c %d %d %d %d", &unused, comm, &state, &ppid, &pgrp, &session, &tty_nr);
+                if (f != NULL)
+                    fscanf(f, "%*d %s %c %d %d %d %d", comm, &state, &ppid, &pgrp, &session, &tty_nr);
                 fclose(f);
-
 
                 printf("pid = %s\n",optarg);
                 printf("comm = %s\n", comm);
@@ -59,7 +95,6 @@ int main(int argc, char *argv[])
                 printf("session = %d\n",session); 
                 break;
             case 's':
-                // parseFile(optarg);
                 // printf("state = %c\n", state);
                 break;
             case 'U':
@@ -79,20 +114,3 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
-
-
-// void parseFile(char *optarg){
-//     if (optarg == NULL){
-//         // sprintf(filename,"/proc/1/stat");
-//         // FILE *f = fopen(filename, "r");
-//         // fscanf(f, "%d %s %c %d %d %d %d", &unused, comm, &state, &ppid, &pgrp, &session, &tty_nr);
-//         // fclose(f);
-//         printf("Hello\n");
-//     }
-//     else{
-//         sprintf(filename,"/proc/%s/stat",optarg);
-//         FILE *f = fopen(filename, "r");
-//         fscanf(f, "%d %s %c %d %d %d %d", &unused, comm, &state, &ppid, &pgrp, &session, &tty_nr);
-//         fclose(f);
-//     }
-// }
