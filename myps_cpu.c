ae95e3ad25763acde30e36f10e4837a2ec4e514d
AdamRa97 <adamra97@gmail.com>
2023-03-02 02:15:15 -0500

    Added /proc/cpuinfo program

--- myps_cpu.c
+++ myps_cpu.c
@@ -1,54 +1,40 @@
 // Adam Ra
-// Purpose : A parser to read into the /proc filesystem
-// Lab 01
+// Purpose : Reporting the number of processors and amount of cache in each CPU
+// Lab 01 Warm Up
 
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
-{
-	int opt, unused, ppid;
-    char filename[1000];
-    char comm[1000];
-    char state;
-    
-    if (argc == 1)
-        printf("No parameters\n");
+/*
+    This is a program that is designed to show how many processors and how big the cache size is for each processor
+    within the /proc/cpuinfo
 
-	while((opt = getopt(argc, argv, "p:sUSvc")) != -1)
-	{
-		switch(opt)
-		{
-			case 'p':
-                printf("pid = %s\n",optarg);
-                sprintf(filename,"/proc/%s/stat",optarg);
-                FILE *f = fopen(filename, "r");
-                fscanf(f, "%d %s %c %d", &unused, comm, &state, &ppid);
-                printf("comm = %s\n", comm);
-                printf("state = %c\n", state);
-                printf("parent pid = %d\n", ppid);
-                fclose(f);
-                break;
-            case 's':
-                printf("s\n");
-                break;
-            case 'U':
-                printf("U\n");
-                break;
-            case 'S':
-                printf("S\n");
-                break;
-            case 'v':
-                printf("v\n");
-                break;
-            case 'c':
-                printf("c\n");
-                break;
-		}
-	}
+    Same technique is used as in Week 3 deliverables to go inside the file / directory
+*/
+int main() {
+    int proc = 0;
+    int cache = 0;
+    char line[1024];
 
-	return 0;
+    // Opening up the file
+    FILE *cpuinfo = fopen("/proc/cpuinfo", "r");
+    if (cpuinfo == NULL)
+        return -1;
+
+    // While there are lines of content go through the loop
+    while (fgets(line, sizeof(line), cpuinfo) != NULL) {
+        // Check for end of CPU section
+        if (strcmp(line, "\n") == 0)
+            printf("Processor #%d cache size is %d KB\n", proc, cache);
+        // Check for processor
+        else if (sscanf(line, "processor : %d", &proc) == 1)
+            continue;
+        // Check for cache size
+        else if (sscanf(line, "cache size : %d", &cache) == 1)
+            continue;
+    }
+
+    fclose(cpuinfo);
+    return 0;
 }
+
